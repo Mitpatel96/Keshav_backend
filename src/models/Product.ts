67f39801fd2e93601b276853f60062834fa76537
amankumar2k24 <aman.kumar2k15@gmail.com>
2025-11-06 22:09:09 +0530

    changes in product model

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -3,6 +3,7 @@ import mongoose, { Schema, Document } from 'mongoose';
 export interface IProduct extends Document {
     title: string;
     description?: string;
+    images: string[];
     isCombo: boolean;
     quantity: number;
     skus: Array<{
@@ -16,6 +17,7 @@ const ProductSchema: Schema = new Schema(
     {
         title: { type: String, required: true },
         description: { type: String },
+        images: [{ type: String }],
         isCombo: { type: Boolean, default: false },
         quantity: { type: Number, default: 1 },
         skus: [{ sku: { type: Schema.Types.ObjectId, ref: 'Sku', required: true }, }],
