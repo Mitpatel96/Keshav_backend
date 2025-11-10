700460ffb28a8a47dcd11c7b05060aa9b6385a5e
amankumar2k24 <aman.kumar2k15@gmail.com>
2025-11-10 21:24:08 +0530

    created api - getProductById

--- src/routes/user/userSideRoutes.ts
+++ src/routes/user/userSideRoutes.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-import { getProducts } from '../../controllers/productController';
+import { getProductById, getProducts } from '../../controllers/productController';
 import { getTraders } from '../../controllers/user/traderController';
 import { getWebsiteSections } from '../../controllers/user/websiteSectionController';
 
 const router = express.Router();
 
 router.get('/products', getProducts);
+router.get('/product/:id', getProductById);
 router.get('/traders', getTraders);
 router.get('/website-sections', getWebsiteSections);
 
